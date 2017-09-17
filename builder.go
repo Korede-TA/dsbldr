fdc849e4f7daf65a4121ca742be0f206d6ac089d
Korede Aderele <korede@n3-11-188.dhcp.drexel.edu>
2017-09-16 23:12:43 -0400

    Fix Updated doc comments

--- builder.go
+++ builder.go
@@ -1,10 +1,10 @@
 package dsbldr
 
-// Builder is main type for this tool, holds all of the
+// Builder is main type for this tool.
 type Builder struct {
 	BaseURL  string // Base social API url to use
 	Primary  PrimaryFeature
-	Features []BaseFeature
+	Features []BaseFeature // Normal features whose retrieval is based on the primnary feature
 }
 
 // Run Builder to aggregate all features and manage concurrency
