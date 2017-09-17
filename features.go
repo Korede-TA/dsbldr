9544d215581dabb7b23d4b0ab31163e7a2b779f6
Korede Aderele <korede@n3-11-188.dhcp.drexel.edu>
2017-09-17 18:39:51 -0400

    Clean up boilerplate, add some utility functionality

--- features.go
+++ features.go
@@ -1,43 +1,18 @@
 package dsbldr
 
-// BaseFeature Interface
-type BaseFeature interface {
-	Run()
-	Deserialize()
-}
+// RunFunc holds the computation that processes the API responses to features
+// is sent a JSON string and returns
+type RunFunc func(data string) []interface{}
 
-// PrimaryFeature in the dataset, on which all other features are based on
-// Implements BaseFeature
-type PrimaryFeature struct {
-	Name        string
-	URLEndpoint string // Enpoint on social API that the feature aggregates data from
-	data        string // JSON string containing data
-}
-
-// Run retrieves the feature via JSON GET requests to the API
-func (pf PrimaryFeature) Run() {
-
-}
-
-// Deserialize returns a map that represents the JSON deserialized data field
-func (pf PrimaryFeature) Deserialize() map[string]interface{} {
-	return nil
-}
-
-// Feature is a Regular dataset feature that is based off the primary field
+// Feature in the dataset, on which all other features are based on
 // Implements BaseFeature
 type Feature struct {
-	Name        string
-	URLEndpoint string // Enpoint on social API that the feature aggregates data from
-	data        string // JSON string containing data
+	Name     string
+	Endpoint string // API Endpoint
+	RunFunc  RunFunc
 }
 
-// Run retrieves the feature via JSON GET requests to the API
-func (f Feature) Run() {
-
-}
-
-// Deserialize returns a map that represents the JSON deserialized data field
-func (f Feature) Deserialize() map[string]interface{} {
-	return nil
+// NewFeature creates new Feature with defaults
+func NewFeature() *Feature {
+	return &Feature{}
 }
