330ca5b6c3a68724194cc4c830b488ae14343fd3
Korede Aderele <kaderele@gmail.com>
2017-09-21 11:28:55 -0400

    Clean up boilerplate, add some utility functionality

--- builder.go
+++ builder.go
@@ -1,18 +1,48 @@
 package dsbldr
 
+import (
+	"io"
+)
+
 // Builder is main type for this tool.
 type Builder struct {
-	BaseURL  string // Base social API url to use
-	Primary  PrimaryFeature
-	Features []BaseFeature // Normal features whose retrieval is based on the primnary feature
+	BaseURL  string // Base API url to use
+	Primary  Feature
+	Features []Feature // Normal features based on primary
 }
 
-// Run Builder to aggregate all features and manage concurrency
-func (b *Builder) Run() {
+// func (b *Builder) buildFeatureTree() {
+// 	// Since datasets might consist of a number of interdependent features
+// 	// The tool will be tasked with building a tree of the features
+
+// }
+
+// Init initializes the Builder
+func (b *Builder) Init() {
 
 }
 
-// GetFeature returns a specific feature in the detaset based on it's name
-func (b *Builder) GetFeature(name string) {
+// Run Builder to aggregate all features and manage concurrent operations
+func (b *Builder) Run() error {
+	return nil
+}
 
+// Save commits the
+func (b *Builder) Save(io.Writer) error {
+	return nil
+}
+
+// AddFeature adds a Feature struct to the "Features" Field on Builder
+func (b *Builder) AddFeature(feature Feature) {
+	b.Features = append(b.Features, feature)
+}
+
+// GetFeature returns a feature in the detaset based on it's name
+func (b *Builder) GetFeature(name string) *Feature {
+	for _, i := range b.Features {
+		if i.Name == name {
+			return &i
+		}
+	}
+	return nil
 }
