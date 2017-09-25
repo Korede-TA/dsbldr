5af33475dc7b02456b981774093dfc9200d8f126
Korede Aderele <kaderele@gmail.com>
2017-09-25 10:30:34 -0400

    Add util function for writing a column to [][]string

--- util.go
+++ util.go
@@ -15,3 +15,19 @@ func BasicOAuthHeader(consumerKey, nonce, signature, signatureMethod,
 		oauth_token="%s`,
 		consumerKey, nonce, signature, signatureMethod, timestamp, token)
 }
+
+func writeStringColumn(data *[][]string, columnName string, values []string) {
+	var colIndex int
+	for i := range (*data)[0] {
+		// Find first empty column
+		if (*data)[0][i] == "" {
+			colIndex = i
+			(*data)[0][i] = columnName
+			break
+		}
+	}
+	// Add all the values as well (remember that Builder.data is pre-allocated)
+	for i := 1; i < len(*data); i++ {
+		(*data)[i][colIndex] = values[i-1]
+	}
+}
