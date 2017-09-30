7da15c4e1ef21a545a81c65d029248a820e8fe7c
Korede Aderele <kaderele@gmail.com>
2017-09-30 17:45:26 -0400

    Change RunFunc signature to accomodate for multiple request string dumps

--- features.go
+++ features.go
@@ -6,12 +6,12 @@ import (
 )
 
 // RunFunc holds the computation that processes the API responses to features
-// is sent a JSON string of the response ??as well as a map of data from the features parent features??
+// is sent an array of JSON strings as the responses ??as well as a map of data from the features parent features??
 // Basically what you do with the run function is take in a string of
 // serialized API data (could be in JSON or XML), do parsing on your
 // own or using utility functions. You do whatever computations you want and
 // then spit it back as an array of strings to read to CSV or JSON
-type RunFunc func(response string) []string // parents map[string]string
+type RunFunc func(responses []string) []string // parents map[string]string
 
 // Structs representing RetreiveType
 // SingleRetrieve Features only require one request to create the JSON Dump
